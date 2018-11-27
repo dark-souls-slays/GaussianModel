a70ec5d7889158cb72f0a0204514c74e11a5bed9
dark-souls-slays <n3m0a3u@gmail.com>
2018-11-27 21:43:49 +0800

    vectors ready

--- hw.py
+++ hw.py
@@ -22,48 +22,22 @@ thresh = cv2.erode(thresh, None, iterations=2)
 thresh = cv2.dilate(thresh, None, iterations=4)
 cv2.imwrite('thresh.png',thresh)
 
-# perform a connected component analysis on the thresholded
-# image, then initialize a mask to store only the "large"
-# components
-labels = measure.label(thresh, neighbors=8, background=0)
-mask = np.zeros(thresh.shape, dtype="uint8")
+np.set_printoptions(threshold=np.inf)
+data = np.array(thresh)
+data_color = np.array(image)
+print(data.shape)
+print(data_color.shape)
 
-# loop over the unique components
-for label in np.unique(labels):
-	# if this is the background label, ignore it
-	if label == 0:
-		continue
+gaussian_data_w0 = np.empty((0, 3))
+gaussian_data_w1 = np.empty((0, 3))
 
-	# otherwise, construct the label mask and count the
-	# number of pixels
-	labelMask = np.zeros(thresh.shape, dtype="uint8")
-	labelMask[labels == label] = 255
-	numPixels = cv2.countNonZero(labelMask)
+for i in range(10):
+	for j in range(5946):
+		if data[i][j] == 255:
+			gaussian_data_w0 = np.append(gaussian_data_w0, [data_color[i][j]], axis = 0)
+		else:
+			gaussian_data_w1 = np.append(gaussian_data_w1, [data_color[i][j]], axis = 0)
+	print(i)
 
-	# if the number of pixels in the component is sufficiently
-	# large, then add it to our mask of "large blobs"
-	if numPixels > 300:
-		mask = cv2.add(mask, labelMask)
-	print("working...")
-# find the contours in the mask, then sort them from left to
-# right
-cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-	cv2.CHAIN_APPROX_SIMPLE)
-cnts = cnts[0] if imutils.is_cv2() else cnts[1]
-cnts = contours.sort_contours(cnts)[0]
 
-# loop over the contours
-for (i, c) in enumerate(cnts):
-	# draw the bright spot on the image
-	(x, y, w, h) = cv2.boundingRect(c)
-	((cX, cY), radius) = cv2.minEnclosingCircle(c)
-	cv2.circle(image, (int(cX), int(cY)), int(radius),
-		(0, 0, 255), 3)
-	cv2.putText(image, "#{}".format(i + 1), (x, y - 15),
-		cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
-	print("working...")
-# show the output image
-cv2.imwrite('image.png',image)
-print("is done")
-#cv2.imshow("Image", image)
-#cv2.waitKey(0)
+#Gaussian distribution
